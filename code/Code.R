cd2c312d9ef7539a7e6745fee69ee7792433171b
Łukasz <homesikgold@wp.pl>
2023-04-23 17:14:46 +0200

    korelacja

--- code/Code.R
+++ code/Code.R
@@ -1,6 +1,14 @@
-for(i in 1:20){
-  print(i)
+dane=data10
+attach(data10)
+summary(dane)
+co=data.frame(dane)
+View(co)
+colnames(co)=c(seq(1,11,by=1))
+cor(co,use = 'complete.obs')
+for(i in 1:10){
+  cat('Korelacja', colnames(co)[1], 'i', colnames(co)[i])
+  print(cor(co[1],dane[i],use = 'complete.obs'))
 }
+cor(dane[1],dane[2])
+cor(total.sum.of.scores.from.6.problems.,Population..total)
 
-
-sdgvxfg=23
