60fd50a668167fc283048f136a636396e66a0963
Łukasz <homesikgold@wp.pl>
2023-04-23 19:37:27 +0200

    poprawka

--- code/Code.R
+++ code/Code.R
@@ -5,10 +5,16 @@ co=data.frame(dane)
 View(co)
 colnames(co)=c(seq(1,11,by=1))
 cor(co,use = 'complete.obs')
+
 for(i in 1:10){
   cat('Korelacja', colnames(co)[1], 'i', colnames(co)[i])
   print(cor(co[1],dane[i],use = 'complete.obs'))
 }
-cor(dane[1],dane[2])
-cor(total.sum.of.scores.from.6.problems.,Population..total)
+total_score=c()
+for(i in 1:11){
+  total_score=append(total_score,cor(dane[1],dane[i],use = 'complete.obs'))
+}
+
+X=data.frame(colnames(dane),total_score)
+
 
